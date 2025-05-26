d27229d1af67cfc33680b6e4ce85cd34852e77dc
Loweso <dlouisefermin@gmail.com>
2025-05-26 15:11:01 +0800

    tabs for favorites and bookings

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -5,12 +5,12 @@ export default function RootLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "#1D4ED8", // Tailwind blue-700
-        tabBarInactiveTintColor: "#6B7280", // Tailwind gray-500
+        tabBarActiveTintColor: "#1D4ED8",
+        tabBarInactiveTintColor: "#6B7280",
         tabBarStyle: {
           backgroundColor: "#FFFFFF",
           borderTopWidth: 1,
-          borderTopColor: "#E5E7EB", // Tailwind gray-200
+          borderTopColor: "#E5E7EB",
           height: 100,
           paddingBottom: 5,
           paddingTop: 5,
@@ -28,7 +28,35 @@ export default function RootLayout() {
           title: "Home",
           tabBarIcon: ({ color, size }) => (
             <Image
-              source={require("@/assets/addlisting/homeWithGirl.png")}
+              source={require("@/assets/tabs/home.png")}
+              className="w-8 h-8"
+              resizeMode="contain"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="bookings"
+        options={{
+          headerShown: false,
+          title: "Bookings",
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("@/assets/tabs/bookings.png")}
+              className="w-8 h-8"
+              resizeMode="contain"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          headerShown: false,
+          title: "Favorites",
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("@/assets/tabs/favorites.png")}
               className="w-8 h-8"
               resizeMode="contain"
             />
